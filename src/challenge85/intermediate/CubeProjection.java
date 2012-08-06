84e0a9e1ea995e57e2c777eabf01f848590c37b8
Filipe Guedes <fgs.guedes@gmail.com>
2012-08-06 15:41:45 -0300

    Challenge 85 - Intermediate - 3D cuboid projection

---
+++ src/challenge85/intermediate/CubeProjection.java
@@ -0,0 +1,70 @@
+package challenge85.intermediate;
+
+/**
+ * [2012/08/03] Challenge #85 [intermediate] (3D cuboid projection)
+ * http://www.reddit.com/r/dailyprogrammer/comments/xq2ao/832012_challenge_85_intermediate_3d_cuboid/
+ *
+ * Write a program that outputs simple 3D ASCII art for a cuboid in an oblique perspective, given a length, height,
+ * and depth, like this:
+ * $ python 3d.py 20 10 3
+ *
+ *    :::::::::::::::::::/
+ *   :::::::::::::::::::/+
+ *  :::::::::::::::::::/++
+ * ####################+++
+ * ####################+++
+ * ####################+++
+ * ####################+++
+ * ####################+++
+ * ####################+++
+ * ####################+++
+ * ####################++
+ * ####################+
+ * ####################
+ *
+ * (The characters used for the faces (here #, :, and +) are fully up to you,
+ * but make sure you don't forget the / on the top-right edge.)
+ *
+ * User: Filipe
+ * Date: 06/08/12
+ */
+public class CubeProjection {
+
+    public static void main(String[] args) {
+
+        drawCube(20, 10, 3);
+
+    }
+
+    public static void drawCube(final int width, final int height, final int depth) {
+
+        for (int i = 0; i < height + depth; i++) {
+
+            for (int j = 0; j < width + depth; j++) {
+
+                if (i < depth) {
+                    if (i + j < depth) {
+                        System.out.print(" ");
+                    } else if (j == width + depth - i - 1) {
+                        System.out.print("/");
+                    } else if (j > width + depth - i - 1) {
+                        System.out.print("+");
+                    } else {
+                        System.out.print(":");
+                    }
+                } else {
+                    if (j < width) {
+                        System.out.print("#");
+                    } else if (i + j <= width + height + depth - 2) {
+                        System.out.print("+");
+                    }
+                }
+
+            }
+
+            System.out.println();
+        }
+
+    }
+
+}
